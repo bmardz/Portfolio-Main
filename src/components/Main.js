48defec45f1369766e9f5143448704a732d4e180
Bryce Mardon <brycemardon@Bryces-MBP.modem>
2020-09-15 14:15:38 +1000

    update files

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,10 @@
-import PropTypes from 'prop-types'
-import React from 'react'
-import pic01 from '../images/pic01.jpg'
-import dp from '../images/dp.jpg'
-import todo from '../images/todo.png'
-import smartbrain from '../images/smartbrain.png'
+import PropTypes from "prop-types";
+import React from "react";
+import pic01 from "../images/pic01.jpg";
+import dp from "../images/dp.jpg";
+import todo from "../images/todo.png";
+import smartbrain from "../images/smartbrain.png";
+import netflixclone from "../images/netflix-clone.png";
 
 class Main extends React.Component {
   render() {
@@ -11,47 +12,37 @@ class Main extends React.Component {
       <div
         className="close"
         onClick={() => {
-          this.props.onCloseArticle()
+          this.props.onCloseArticle();
         }}
       ></div>
-    )
+    );
 
     return (
       <div
         ref={this.props.setWrapperRef}
         id="main"
-        style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}
+        style={this.props.timeout ? { display: "flex" } : { display: "none" }}
       >
         <article
           id="intro"
-          className={`${this.props.article === 'intro' ? 'active' : ''} ${
-            this.props.articleTimeout ? 'timeout' : ''
+          className={`${this.props.article === "intro" ? "active" : ""} ${
+            this.props.articleTimeout ? "timeout" : ""
           }`}
-          style={{ display: 'none' }}
+          style={{ display: "none" }}
         >
           <h2 className="major">Netflix Clone</h2>
           <span className="image main">
-            <img src={pic01} alt="" />
+            <img src={netflixclone} alt="" />
           </span>
           <p>
-            Built a Slack clone where users can send messages to one another within the app. Messages are stored on Firebase so users can see old messages when reopening the app.
-            </p>
-            <br></br>
-            <p>
-            React - Firebase <a href="https://github.com/bmardz/slack-clone">Live version</a>.
+            Netflix clone that imitates the home page of Netflix and is deployed
+            live through firebase. Use of TMDB API key to reach movie database
+            and display categorised movies under genres.
           </p>
-          {close}
-
-          <h2 className="major">Spotify Clone</h2>
-          <span className="image main">
-            <img src={pic01} alt="" />
-          </span>
+          <br></br>
           <p>
-            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-            By the way, check out my <a href="#work">Live Version</a>.
+            React - Firebase{" "}
+            <a href="https://github.com/bmardz/netflix-clone">Live version</a>.
           </p>
           {close}
 
@@ -60,16 +51,20 @@ class Main extends React.Component {
             <img src={smartbrain} alt="" />
           </span>
           <p>
-            A front-end web application that allows users to pretend to sign in or register. User is then taken to a page that allows them to enter an image url in which the app then detects any faces that are within the image through the use of an API.
+            A front-end web application that allows users to pretend to sign in
+            or register. User is then taken to a page that allows them to enter
+            an image url in which the app then detects any faces that are within
+            the image through the use of an API.
             <br></br>
             <br></br>
             <p>
               Stack
-              <br></br>
-              - Reactjs - 
+              <br></br>- Reactjs -
             </p>
             <p>
-            By the way, check out my <a href="https://github.com/bmardz/Smart-Brain">Github</a> to access the files for this project.
+              By the way, check out my{" "}
+              <a href="https://github.com/bmardz/Smart-Brain">Github</a> to
+              access the files for this project.
             </p>
           </p>
           {close}
@@ -79,61 +74,69 @@ class Main extends React.Component {
             <img src={todo} alt="" />
           </span>
           <p>
-            Simple Todo list built with Javascript. Users are able to do the following:
-            <br></br> 
+            Simple Todo list built with Javascript. Users are able to do the
+            following:
             <br></br>
-              1. Add and delete todo items
             <br></br>
-              2. Select between all, completed or uncompleted list items
+            1. Add and delete todo items
             <br></br>
-              3. Save to local storage so list items still display when browser is reopened
+            2. Select between all, completed or uncompleted list items
+            <br></br>
+            3. Save to local storage so list items still display when browser is
+            reopened
             <br></br>
             <br></br>
             <p>
               Stack
-              <br></br>
-              - Javascript - HTML - CSS
+              <br></br>- Javascript - HTML - CSS
             </p>
             <p>
-              By the way, check out my <a href="https://bmardz.github.io/todo-list/" target="_black">Live Version</a>.
+              By the way, check out my{" "}
+              <a href="https://bmardz.github.io/todo-list/" target="_black">
+                Live Version
+              </a>
+              .
             </p>
           </p>
           {close}
         </article>
 
-        
-
         <article
           id="about"
-          className={`${this.props.article === 'about' ? 'active' : ''} ${
-            this.props.articleTimeout ? 'timeout' : ''
+          className={`${this.props.article === "about" ? "active" : ""} ${
+            this.props.articleTimeout ? "timeout" : ""
           }`}
-          style={{ display: 'none' }}
+          style={{ display: "none" }}
         >
           <h2 className="major">About</h2>
           <span className="image main">
             <img src={dp} alt="" />
           </span>
           <p>
-            Hi, I am Bryce a self taught front end developer based in Wollongong in search for a dev role. Approximately 10 months ago I found a new passion for web development and began teaching myself before and after work. 
+            Hi, I am Bryce a self taught front end developer based in Wollongong
+            in search for a dev role. Approximately 10 months ago I found a new
+            passion for web development and began teaching myself before and
+            after work.
             <br></br>
-            <br></br>
-            I love problem solving and building projects spending most of my free time coding and learning new things to become a better developer. I am hard working, passionate, committed and a fast learner! 
+            <br></br>I love problem solving and building projects spending most
+            of my free time coding and learning new things to become a better
+            developer. I am hard working, passionate, committed and a fast
+            learner!
           </p>
           <br></br>
           <p>
             My current skills include:
             <br></br>
             <br></br>
-            HTML - CSS - Javascript - Reactjs - Redux - Firebase - Gatsby - VSCode
+            HTML - CSS - Javascript - Reactjs - Redux - Firebase - Gatsby -
+            VSCode
           </p>
           {close}
         </article>
       </div>
-    )
+    );
   }
 }
-
 
 Main.propTypes = {
   route: PropTypes.object,
@@ -142,6 +145,6 @@ Main.propTypes = {
   onCloseArticle: PropTypes.func,
   timeout: PropTypes.bool,
   setWrapperRef: PropTypes.func.isRequired,
-}
+};
 
-export default Main
+export default Main;
